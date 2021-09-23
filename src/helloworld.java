4fd08ad07163e38e9e1168d6688536ab11ff73c8
SmartXiaoFeng <2206164777@qq.com>
2021-09-23 16:09:58 +0800

    Initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        ArrayList<Integer> l1 = new ArrayList<>();
+        int a = 1;
+        int b = 11;
+        l1.add(1);
+    }
+}
